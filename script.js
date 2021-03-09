f6fa363e6c7dc795bfccb9736ffb6f7f70ea3a09
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-09 18:51:20 -0300

    Adicionado o 9º requisito

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 */
 const colorArray = document.querySelectorAll('.color');
 const colorPalette = document.getElementById('color-palette');
+const clearButton = document.querySelector('button');
+const pixelArray = document.querySelectorAll('.pixel');
 colorPalette.addEventListener('click', (event) => {
   for (let index = 0; index < colorArray.length; index += 1) {
     if (colorArray[index].className === 'color selected') {
@@ -21,5 +23,14 @@ document.addEventListener('click', (event) => {
   const target = event.target;
   if (target.classList.contains('pixel')) {
     target.style.backgroundColor = colorSelected;
+  }
+}, false);
+
+clearButton.addEventListener('click', () => {
+  for (let index = 0; index < pixelArray.length; index += 1) {
+    let bgColorPixelArrayItem = pixelArray[index].style.backgroundColor;
+    if (bgColorPixelArrayItem !== '') {
+      pixelArray[index].style.backgroundColor = '';
+    }
   }
 }, false);
