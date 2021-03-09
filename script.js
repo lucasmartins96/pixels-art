1f52a326fc7d326ce1168b256d72005e38604eb7
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-09 17:45:33 -0300

    Adicionado o 8º requisito

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+/*
+* Recorri ao seguinte site afim de utilizar o borbulhamento de eventos.
+* Link: https://gomakethings.com/attaching-multiple-elements-to-a-single-event-listener-in-vanilla-js/
+*/
 const colorArray = document.querySelectorAll('.color');
-document.addEventListener('click', (event) => {
+const colorPalette = document.getElementById('color-palette');
+colorPalette.addEventListener('click', (event) => {
   for (let index = 0; index < colorArray.length; index += 1) {
     if (colorArray[index].className === 'color selected') {
       colorArray[index].className = 'color';
@@ -8,5 +13,13 @@ document.addEventListener('click', (event) => {
 
   if (event.target.classList.contains('color')) {
     event.target.className = 'color selected';
+  }
+}, false);
+
+document.addEventListener('click', (event) => {
+  let colorSelected = document.querySelector('.selected').id;
+  let target = event.target;
+  if (target.classList.contains('pixel')) {
+    target.style.backgroundColor = colorSelected;
   }
 }, false);
