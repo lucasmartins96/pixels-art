81c1bf28a37dfd9c43f3c590204b474b0a3ca06d
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-10 00:16:58 -0300

    Adicionado o 10º requisito

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 * Recorri ao seguinte site afim de utilizar o borbulhamento de eventos.
 * Link: https://gomakethings.com/attaching-multiple-elements-to-a-single-event-listener-in-vanilla-js/
 */
+
 const colorArray = document.querySelectorAll('.color');
 const colorPalette = document.getElementById('color-palette');
 const clearButton = document.querySelector('button');
-const pixelArray = document.querySelectorAll('.pixel');
+const pixelBoard = document.getElementById('pixel-board');
+const vqvButton = document.getElementById('generate-board');
+const inputBoardSize = document.getElementById('board-size');
+
+pixelBoard.innerHTML = '';
+
 colorPalette.addEventListener('click', (event) => {
   for (let index = 0; index < colorArray.length; index += 1) {
     if (colorArray[index].className === 'color selected') {
@@ -16,6 +22,16 @@ colorPalette.addEventListener('click', (event) => {
   if (event.target.classList.contains('color')) {
     event.target.className = 'color selected';
   }
+  /*
+  function selectedPalette() {
+const color = document.querySelector('#color-palette');
+color.addEventListener('click', (event) => {
+const element = event;
+document.querySelector('.selected').className = 'color';
+element.target.className = 'color selected';
+});
+} 
+  */
 }, false);
 
 document.addEventListener('click', (event) => {
@@ -27,10 +43,26 @@ document.addEventListener('click', (event) => {
 }, false);
 
 clearButton.addEventListener('click', () => {
+  const pixelArray = document.querySelectorAll('.pixel');
   for (let index = 0; index < pixelArray.length; index += 1) {
     let bgColorPixelArrayItem = pixelArray[index].style.backgroundColor;
     if (bgColorPixelArrayItem !== '') {
       pixelArray[index].style.backgroundColor = '';
+    }
+  }
+}, false);
+
+vqvButton.addEventListener('click', () => {
+  pixelBoard.innerHTML = '';
+  const sizeValue = parseInt(inputBoardSize.value, 10);
+  for (let index = 0; index < sizeValue; index += 1) {
+    let trClass = document.createElement('div');
+    trClass.className = 'tr';
+    pixelBoard.appendChild(trClass);
+    for (let index2 = 0; index2 < sizeValue; index2 += 1) {
+      let tdClass = document.createElement('div');
+      tdClass.className = 'td pixel';
+      trClass.appendChild(tdClass);
     }
   }
 }, false);
